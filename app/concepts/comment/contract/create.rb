0fcaf9fe85b0f4b4a4120b9c003f801ede01ae13
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-07-22 16:00:23 +0600

    * move logic in new form into form class

--- app/concepts/comment/contract/create.rb
+++ app/concepts/comment/contract/create.rb
@@ -2,12 +2,20 @@ module Comment::Contract
   class Create < Reform::Form
     include Reform::Form::ActiveRecord
 
+    def self.weights
+      { '0' => 'Nice!', '1' => 'Rubbish!'}
+    end
+
+    def weights
+      [self.class.weights.to_a, :first, :last]
+    end
+
     property :body
     property :weight, prepopulator: ->(*) { self.weight = 0 }
     property :thing
 
     validates :body, length: { in: 6..160 }
-    validates :weight, inclusion: { in: ['0', '1'] }
+    validates :weight, inclusion: { in: weights.keys }
     validates :thing, :user, presence: true
 
     property :user,
